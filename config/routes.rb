150fb6cb7ef71f412aa7c89ad26e261e147da8d8
Bruno <brunofiliperamos@Hotmail.com>
2019-07-03 20:17:40 -0300

    Criado primeira página e definido ela como root

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  
+  # Acho que define o controller welcome e definição index como a url / do site 
+  root 'welcome#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
